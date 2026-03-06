574e656332d6f67a954420112d9f6fe6bdf06f7c
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-03-06 21:53:53 +0530

    feat: implement express server with MongoDB connection and update package.json scripts 😎

--- online-clothing-contactUs-service/index.js
+++ online-clothing-contactUs-service/index.js
@@ -1,2 +1,23 @@
 import express from "express";
-import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+let mongoUrl = process.env.MONGO_URL;
+mongoose.connect(mongoUrl);
+
+let connection =  mongoose.connection
+connection.once("open", ()=>{
+    console.log("MongoDB connected successfully ✅")
+});
+
+app.listen(3002,()=>{
+    console.log("Server is running on PORT 3000 🚀")
+});
