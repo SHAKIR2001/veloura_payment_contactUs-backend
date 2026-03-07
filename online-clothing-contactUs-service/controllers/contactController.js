cb25cc3d3588251597c33cd57ffff6e89a57dab1
SHAKIR CASSIM <shakircassim077@gmail.com>
2026-03-07 14:15:35 +0530

    feat: add functions to retrieve and resolve contact messages in the contact controller😎

--- online-clothing-contactUs-service/controllers/contactController.js
+++ online-clothing-contactUs-service/controllers/contactController.js
@@ -29,3 +29,31 @@ export async function createContactMessage(req, res) {
         res.status(500).json({ message: "Message cannot be sent" });
     }
 }
+
+export async function getAllContactMessages(req, res) {
+    try {
+        const messages = await ContactMessage.find().sort({ createdAt: -1 });
+        res.status(200).json(messages);
+    } catch (e) {
+        res.status(500).json({ message: "Failed to fetch messages" });
+    }
+}
+
+export async function resolveContactMessage(req, res) {
+    try {
+        const { id } = req.params;
+        const updated = await ContactMessage.findOneAndUpdate(
+            { id: Number(id) },
+            { isResolved: true },
+            { new: true }
+        );
+
+        if (!updated) {
+            return res.status(404).json({ message: "Message not found" });
+        }
+
+        res.status(200).json({ message: "Marked as resolved", data: updated });
+    } catch (e) {
+        res.status(500).json({ message: "Failed to resolve message" });
+    }
+}
--- online-clothing-contactUs-service/routes/contactRouter.js
+++ online-clothing-contactUs-service/routes/contactRouter.js
@@ -1,6 +1,10 @@
 import express from "express";
-import { createContactMessage } from "../controllers/contactController.js";
+import { createContactMessage, getAllContactMessages, resolveContactMessage  } from "../controllers/contactController.js";
 
 const contactRouter = express.Router();
 
 contactRouter.post("/", createContactMessage);
+contactRouter.get("/", verifyAdmin, getAllContactMessages);            
+contactRouter.patch("/:id/resolve", verifyAdmin, resolveContactMessage);
+
+export default contactRouter;
